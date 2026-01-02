9f8924f28d3f04f6802d0f10d8af9c13e4e4e2a9
Ivan Timofeev <purplebaby16@vk.com>
2026-01-02 20:06:34 +0300

    Minor

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,14 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
-import { Analytics } from "@vercel/analytics/next"
 import "./globals.css"
 
 const _geist = Geist({ subsets: ["latin"] })
 const _geistMono = Geist_Mono({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Виш-лист | Список желаний",
-  description: "Создавайте и управляйте вашими желаниями",
+  title: "Хочу вкусняшек | Заказики",
+  description: "Заказывай, что хочешь (ну почти)",
   generator: "v0.app",
   icons: {
     icon: [
@@ -39,7 +38,6 @@ export default function RootLayout({
     <html lang="ru">
       <body className={`font-sans antialiased`}>
         {children}
-        <Analytics />
       </body>
     </html>
   )
